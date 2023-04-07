8288857c51269f53e85dbf3e0f2751b4064e34ab
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-04-07 22:32:58 +0300

    updated

---
+++ MvcCoreCamp/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,100 @@
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreCamp.Areas.Admin.Models;
+using MvcCoreCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCamp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var x in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = x.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = x.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+        }
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+                new BlogModel { ID=1,BlogName="C# Programlama"},
+                new BlogModel{ID=2,BlogName="Tesla Araçları"},
+                new BlogModel{ID=3,BlogName="2023 Olimpiyatları"}
+            };
+            return bm;
+        }
+
+        public IActionResult BLogListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDynamicExcellBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var x in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = x.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = x.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+
+            }
+        }
+
+        public List<BlogModel2> BlogTitleList()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using (var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel2
+                {
+                    ID = x.BlogID,
+                    BlogName = x.BlogTitle
+                }).ToList();
+            }
+            return bm;
+        }
+        public IActionResult BlogTitleListExcel()
+        {
+            return View();
+        }
+
+    }
+}
