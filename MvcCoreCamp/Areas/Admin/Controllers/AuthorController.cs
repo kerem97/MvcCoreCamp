0bfe712d3a2aa4050804f0f5cd4a08d09361d49a
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-04-08 15:37:12 +0300

    updated-json-identity and api

---
+++ MvcCoreCamp/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreCamp.Areas.Admin.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCamp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AuthorController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AuthorList()
+        {
+            var jsonAuthors = JsonConvert.SerializeObject(authors);
+            return Json(jsonAuthors);
+        }
+
+        public IActionResult GetAuthorByID(int authorid)
+        {
+            var findAuthor = authors.FirstOrDefault(x => x.ID == authorid);
+            var jsonAuthors = JsonConvert.SerializeObject(findAuthor);
+            return Json(jsonAuthors);
+        }
+
+        public static List<AuthorClass> authors = new List<AuthorClass>
+        {
+            new AuthorClass
+            {
+                ID=1,
+                Name="Ayşe"
+            },
+            new AuthorClass
+            {
+                ID=2,
+                Name="Ahmet"
+            }
+        };
+    }
+}
--- MvcCoreCamp/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ MvcCoreCamp/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MvcCoreCamp.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,11 @@ namespace MvcCoreCamp.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.TGetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            //string api = "6182eee38a0b69e4b579f4b59695362f";
+            //string connection = "http://api.openweathermap.org/data/2.5/weather?q=melk&mode=xml&lang=at&units=metric&appid=" + api;
+            //XDocument document = XDocument.Load(connection);
+            //ViewBag.v4 = document.Descendants("temperature ").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
